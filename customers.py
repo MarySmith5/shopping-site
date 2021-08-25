69c7dd2547dc4ef826fcfcd0e2c7bd0d99bde2bd
Mary Smith <mary.sunshine4@gmail.com>
2021-08-25 02:31:46 +0000

    Lots of updates

--- customers.py
+++ customers.py
@@ -5,6 +5,13 @@ class Customer(object):
     """Ubermelon customer."""
 
     # TODO: need to implement this
+"""Customers at Hackbright."""
+
+
+class Customer(object):
+    """Ubermelon customer."""
+
+    # TODO: need to implement this
     def __init__(self,
                  first_name,
                  last_name,
@@ -46,16 +53,13 @@ def read_customers_from_file(filepath):
     return customers
 
 
-def get_all_customers():
-    """Returns a list of all customer"""
-
-    return list(customers.values())
-
 
 def get_by_email(email):
     """Return a customer, given their email"""
-
-    return customers[email]
+    if customers.get(email) != None:
+        return customers[email]
+    else:
+        return False
 
 
 customers = read_customers_from_file("customers.txt")
